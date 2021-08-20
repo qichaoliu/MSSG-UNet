c63432e5cf790ff243d1411b9fdad5358a6929ba
L <19650451+qichaoliu@users.noreply.github.com>
2021-08-20 21:52:11 +0800

    Update MSSGU.py
    Fix some bugs

--- MSSGU.py
+++ MSSGU.py
@@ -12,9 +12,9 @@ class GCNLayer_PyG(nn.Module):
         super(GCNLayer_PyG, self).__init__()
         self.BN = nn.BatchNorm1d(input_dim)
         self.Activition1 = nn.LeakyReLU(inplace=True)
-        # 第一层GCN
-        self.GCN_liner_out_1 = nn.Sequential(nn.Linear(input_dim, output_dim))#,nn.LeakyReLU(inplace=True)
-        self.GCN_liner_theta_1 = nn.Sequential(nn.Linear(input_dim, 128),nn.LeakyReLU(inplace=True))#,nn.ReLU()
+
+        self.GCN_liner_out_1 = nn.Sequential(nn.Linear(input_dim, output_dim))
+        self.GCN_liner_theta_1 = nn.Sequential(nn.Linear(input_dim, 128))
         
         self.position=position
         self.neighbors = neighbors
@@ -26,7 +26,7 @@ class GCNLayer_PyG(nn.Module):
         self.theta1 = nn.Sequential(nn.Linear(input_dim, 1))#,nn.Sigmoid()
         if self.neighbors>0:
             self.neighbors=self.neighbors+1 # self-loop
-            self.col, self.row = self.edge_index = knn_graph(self.position, self.neighbors, batch=None, loop=True)  # row已经排好序 [0,node_count*4) 升序
+            self.col, self.row = self.edge_index = knn_graph(self.position, self.neighbors, batch=None, loop=True)  # 
         else:
             # unfixed neighbors
             self.I = torch.eye(adjacency_matrix.shape[0], adjacency_matrix.shape[0], requires_grad=False, device=device, dtype=torch.float32)
@@ -60,7 +60,7 @@ class GCNLayer_PyG(nn.Module):
             ###################### Softmax
             e = torch.sigmoid(torch.matmul(H_xx1, H_xx1.t()))
             zero_vec = -9e15 * torch.ones_like(e)
-            A = torch.where(self.mask > 0, e, zero_vec)*(1-self.I)+ self.lambda_*self.I
+            A = torch.where(self.mask > 0, e, zero_vec)+ self.lambda_*self.I
             A_softmax = F.softmax(A, dim=1)
             out = self.Activition1(torch.mm(A_softmax, out))
 
@@ -87,18 +87,15 @@ class SSConv(nn.Module):
             stride=1,
             padding=0,
             groups=1,
-            bias=False
         )
-        # self.Act1 = nn.LeakyReLU()
+        self.Act1 = nn.LeakyReLU(inplace=True)
         self.Act2 = nn.LeakyReLU(inplace=True)
-
-        
         self.BN = nn.BatchNorm2d(in_ch)
         # self.BN2 = nn.BatchNorm2d(out_ch)
-    
+
     def forward(self, input):
         out = self.point_conv(self.BN(input))
-        # out = self.Act1(out)
+        out = self.Act1(out)
         # out=self.BN2(out)
         out = self.depth_conv(out)
         out = self.Act2(out)
@@ -108,23 +105,20 @@ class HiGCN(nn.Module):
     def __init__(self, height: int, width: int, changel: int, class_count: int,
                  hierarchy_matrices, adjacency_matrices,neighbors):
         super(HiGCN, self).__init__()
-        # 类别数,即网络最终输出通道数
-        self.class_count = class_count  # 类别数
-        # 网络输入数据大小
+        self.class_count = class_count  
         self.channel = changel
         self.height = height
         self.width = width
         self.S_list = hierarchy_matrices
         self.A_list = adjacency_matrices
-        self.layer_count=len(hierarchy_matrices) # 有多少图卷积层-pooling层
+        self.layer_count=len(hierarchy_matrices) 
         self.neighbors = neighbors
         self.S_list_Hat_T = []
         for i in range(len(hierarchy_matrices)):
             temp=hierarchy_matrices[i]
-            self.S_list_Hat_T.append((temp/ (torch.sum(temp, 0, keepdim=True,dtype=torch.float32))).t())  # 列归一化Q
+            self.S_list_Hat_T.append((temp/ (torch.sum(temp, 0, keepdim=True,dtype=torch.float32))).t())  # Q
         
-        ## 计算每层的所有节点的坐标,用于构建KNN
-        positions = self.getPositions() # 每个节点的坐标
+        positions = self.getPositions()
         self.positions=[]
         for i in range(len(hierarchy_matrices)):
             positions = torch.mm(self.S_list_Hat_T[i], positions)
@@ -137,40 +131,27 @@ class HiGCN(nn.Module):
             layer_channels=layer_channels//2
             layers_per_depth.append(layer_channels)
 
-        # 第一层 使用SSConv模拟像素级图卷积
         self.CNN_head_layer=SSConv(self.channel, layers_per_depth[0], kernel_size=5)
 
-        # 后续层，根据给定池化矩阵个数，定义层数
         self.GCN_layers=nn.Sequential()
         for i in range(self.layer_count):
-            # self.GCN_layers.add_module('my_GCN_layer'+str(i), GCNLayer(layer_channels,layer_channels,
-            #                                                            self.S_list_Hat_T[i],
-            #                                                            self.A_list[i]))
             self.GCN_layers.add_module('my_GCN_layer'+str(i), GCNLayer_PyG(layers_per_depth[i],layers_per_depth[i+1],
                                                                        self.A_list[i],
                                                                        self.positions[i],self.neighbors))
             
-        
         self.De_GCN_layers=nn.Sequential()
         for i in range(self.layer_count-1):
-            # self.De_GCN_layers.add_module('my_DeGCN_layer'+str(i), GCNLayer(2*layer_channels,layer_channels,
-            #                                                                 self.S_list[self.layer_count-i-1],
-            #                                                                 self.A_list[self.layer_count-i-2]))
             self.De_GCN_layers.add_module('my_DeGCN_layer'+str(i), GCNLayer_PyG(layers_per_depth[-i-1]+layers_per_depth[-i-2],layers_per_depth[-i-2],
                                                                                 # layers_per_depth[-i - 1], layers_per_depth[-i - 2],
                                                                             self.A_list[-i-2],
                                                                             self.positions[self.layer_count-i-2],self.neighbors))
         
-        
-        # 最后一层 使用SSConv模拟像素级图卷积
         if len(layers_per_depth)<=1: layers_per_depth.append(layer_channels)
         self.CNN_tail_layer=SSConv(layers_per_depth[0]+layers_per_depth[1],layers_per_depth[0], kernel_size=5)
         self.Softmax = nn.Sequential(nn.Linear(layers_per_depth[0], self.class_count), nn.Softmax(-1))
 
-        
-    
     def getPositions(self):
-        # 构造坐标矩阵,用于计算KNN边
+        # for KNN
         x=torch.arange(0,self.height)
         y=torch.arange(0,self.width)
         x,y = torch.meshgrid([x,y])
@@ -185,47 +166,33 @@ class HiGCN(nn.Module):
         '''
         (h, w, c) = x.shape
         x=torch.unsqueeze(x.permute([2, 0, 1]), 0)
-        
-        # Head CNN
         H_0 = self.CNN_head_layer(x)
         H_0 = torch.squeeze(H_0, 0).permute([1, 2, 0])
-
-        # 数据转换 CNN->GCN
         H_i= H_0.reshape([h * w, -1])
         encoder_features =[]
         encoder_features.append(H_i)
         
-        decoder_features = [] # 用于记录解码器中的特征
-        
-        # 记录编解码器中的邻接矩阵
+        decoder_features = [] #
         A_encoders=[]; A_decoders=[]
         
         # encoder GCN
         for i in range(len(self.GCN_layers)):
             H_i = torch.mm(self.S_list_Hat_T[i], H_i)
-            # H_i = self.GCN_layers[i](H_i,prior_A=edgeMapList[i]) ################
-            H_i,A_i = self.GCN_layers[i](H_i,prior_A=None) ################
+            H_i,A_i = self.GCN_layers[i](H_i,prior_A=None) 
             encoder_features.append(H_i)
             A_encoders.append(A_i)
             
-        A_decoders.append(A_encoders[-1])# decoder 与encoder 最后一层 为同一个
+        A_decoders.append(A_encoders[-1])# decoder encoder 
         decoder_features.append(H_i)
         
-        # for experiment 阻断某个skip connection #####################################
-        # encoder_idx=0
-        # encoder_features[encoder_idx]=encoder_features[encoder_idx]*0
-        # encoder_features[encoder_idx]=encoder_features[encoder_idx].detach()
-        # decoder GCN
         for i in range(len(self.De_GCN_layers)):
             H_i=torch.mm(self.S_list[-i-1], H_i)
-            H_i=torch.cat([H_i,encoder_features[-i-2]],dim=-1)  # concat 模式
-            # H_i=H_i+encoder_features[-i-2]  # add 模式
-            # H_i = self.De_GCN_layers[i]( H_i ,prior_A=edgeMapList[-i-2]) ################
-            H_i,A_i = self.De_GCN_layers[i]( H_i ,prior_A=None) ################
+            H_i=torch.cat([H_i,encoder_features[-i-2]],dim=-1)  # concat 
+            # H_i=H_i+encoder_features[-i-2]  # add 
+            H_i,A_i = self.De_GCN_layers[i]( H_i ,prior_A=None) 
             A_decoders.append(A_i)
             decoder_features.append(H_i)
 
-        # 数据转换 GCN->CNN
         if len(self.S_list)>0: H_i=torch.mm(self.S_list[0],H_i) # hw*d
         H_i = torch.cat([H_i, encoder_features[0]],dim=-1) # hw*(2d) concat 模式
 
@@ -235,7 +202,7 @@ class HiGCN(nn.Module):
         
         # softmax
         final_features=torch.squeeze(final_features, 0).permute([1, 2, 0]).reshape([h * w, -1])
-        decoder_features.append(final_features)  ####################
+        decoder_features.append(final_features)  
         Y = self.Softmax(final_features)
         
         return Y, encoder_features, decoder_features
